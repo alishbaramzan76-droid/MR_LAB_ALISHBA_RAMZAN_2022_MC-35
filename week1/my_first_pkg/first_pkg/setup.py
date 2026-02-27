9850db4e6e7579f15c5d9492b89a58a7d8f90679
alishbaramzan <alishbaramzan76@gmail.com>
2026-02-27 12:12:31 +0500

    Add files via upload

---
+++ week1/my_first_pkg/first_pkg/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'first_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='alishba',
+    maintainer_email='alishba@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+		'simple_node = first_pkg.simple_node:main',
+        ],
+    },
+)
